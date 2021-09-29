35c2bba5f7d5ca0f43dcf282d19215828220ea99
mineLdiver <aabesedin@mail.ru>
2021-09-29 17:40:46 +0500

    Revert "Using StAPI's LevelEvent.Init for hooking into level initialization, as <init>* mixin had some issues."
    This reverts commit 9481218a16a2456c1d12d6b94806963afa469ded.

---
+++ src/main/java/net/mine_diver/smoothbeta/mixin/MixinLevel.java
@@ -0,0 +1,18 @@
+package net.mine_diver.smoothbeta.mixin;
+
+import net.minecraft.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Level.class)
+public class MixinLevel {
+
+    @ModifyConstant(
+            method = "<init>*",
+            constant = @Constant(intValue = 40)
+    )
+    private int modSaveTime(int constant) {
+        return 3600;
+    }
+}
