20b6d4dff88a1e365ea282ea110a00f7cf89a496
mineLdiver <aabesedin@mail.ru>
2023-08-20 01:24:47 +0500

    Fixed Advanced OpenGL

--- src/main/java/net/mine_diver/smoothbeta/mixin/client/multidraw/nop/ChunkRendererMixin.java
+++ src/main/java/net/mine_diver/smoothbeta/mixin/client/multidraw/nop/ChunkRendererMixin.java
@@ -1,7 +1,6 @@
 package net.mine_diver.smoothbeta.mixin.client.multidraw.nop;
 
 import net.minecraft.class_66;
-import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -9,10 +8,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(class_66.class)
 public class ChunkRendererMixin {
     @Redirect(
-            method = {
-                    "method_298",
-                    "method_296"
-            },
+            method = "method_296",
             at = @At(
                     value = "INVOKE",
                     target = "Lorg/lwjgl/opengl/GL11;glNewList(II)V",
@@ -22,30 +18,7 @@ public class ChunkRendererMixin {
     private void smoothbeta_nop_GL11_glNewList(int list, int mode) {}
 
     @Redirect(
-            method = "method_298",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/util/math/Box;getOrCreate(DDDDDD)Lnet/minecraft/util/math/Box;"
-            )
-    )
-    private Box smoothbeta_nop_Box_getOrCreate(double d, double e, double f, double g, double h, double i) {
-        return null;
-    }
-
-    @Redirect(
-            method = "method_298",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/item/ItemRenderer;method_2024(Lnet/minecraft/util/math/Box;)V"
-            )
-    )
-    private void smoothbeta_nop_ItemRenderer_method_2024(Box box) {}
-
-    @Redirect(
-            method = {
-                    "method_298",
-                    "method_296"
-            },
+            method = "method_296",
             at = @At(
                     value = "INVOKE",
                     target = "Lorg/lwjgl/opengl/GL11;glEndList()V",
@@ -96,14 +69,4 @@ public class ChunkRendererMixin {
             )
     )
     private void smoothbeta_nop_GL11_glPopMatrix() {}
-
-    @Redirect(
-            method = "method_303",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lorg/lwjgl/opengl/GL11;glCallList(I)V",
-                    remap = false
-            )
-    )
-    private void smoothbeta_nop_GL11_glCallList(int list) {}
 }
